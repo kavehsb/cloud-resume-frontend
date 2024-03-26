5bb2953846fe98bbad2dc44dd45de1a8529592a6
Kaveh Buenaventura <kavehsbuen@gmail.com>
2024-03-26 01:30:56 -0700

    Change script functionality to send a POST request to the API gateway

--- scripts/visitorcount.js
+++ scripts/visitorcount.js
@@ -1,8 +1,18 @@
 function updateVisitorCount() {
-	let visitorCount = localStorage.getItem("visitorCount") || 0;
-	visitorCount = parseInt(visitorCount) + 1;
-	localStorage.setItem("visitorCount", visitorCount);
-	document.getElementById("visitorCount").textContent = visitorCount;
+	const url = "https://3qjnb2btqj.execute-api.us-west-2.amazonaws.com/test/incrementCount"
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log('Success:', data);
+		// Update the visitor count in the HTML
+		let visitorCount = data.body;
+		document.getElementById("visitorCount").textContent = visitorCount;
+	});
 }
 
 window.addEventListener("load", updateVisitorCount);
